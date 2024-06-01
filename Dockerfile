98cf420c07724c72ec2dc7bb423caf713806bac1
Al Caprar <11180418+alcaprar@users.noreply.github.com>
2024-06-01 18:39:38 +0200

    adapt docker to express

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,11 @@ COPY package.json package-lock.json postcss.config.js tailwind.config.js vite.co
 RUN npm install
 
 COPY src /app/src
+COPY server/index.js /app/server/index.js
 COPY public /app/public
-COPY functions /app/functions
 COPY index.html /app/
 COPY entrypoint.sh /app/
+
+RUN npm run build
 
 ENTRYPOINT /app/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ if [ -z "${GITHUB_CLIENT_SECRET-}" ]; then
    exit 1
 fi
 
-BASE_URL="${BASE_URL:-http://localhost:8788}"
+BASE_URL="${BASE_URL:-http://localhost:3000}"
 
-CONFIG_FILE=".dev.vars"
+CONFIG_FILE=".env"
 touch $CONFIG_FILE
 echo "BASE_URL = \"${BASE_URL}\"" > $CONFIG_FILE
 echo "GITHUB_CLIENT_ID = \"${GITHUB_CLIENT_ID}\"" >> $CONFIG_FILE
 echo "GITHUB_CLIENT_SECRET = \"${GITHUB_CLIENT_SECRET}\"" >> $CONFIG_FILE
 
-npm run dev
+node server/index.js
